e8efe073c5d2d2779d6f3a29a42cc550d74b6f4d
tejaspatil1936 <tejaspatil1936@gmail.com>
2024-08-09 11:20:48 +0530

    codes which never were commited

---
+++ Lecture/asdfghjkl;'/script.js
@@ -0,0 +1,22 @@
+// function print() {
+//     console.log('hello this is function without parameters ');
+// }
+
+// print()
+
+function add(x, y) {
+    return x + y;
+}
+
+let z = add(5, 6);
+
+console.log( 'sum is ' + z);
+
+let a;
+
+function print(value) {
+    let a = value;
+    console.log(a);
+}
+print(5)
+console.log(a);
